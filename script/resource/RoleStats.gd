c5e98ec8111304c59772225213fda0797f05dfec
wsp <483041241@qq.com>
2025-06-08 19:55:03 +0800

    基本人物面板

--- script/resource/RoleStats.gd
+++ script/resource/RoleStats.gd
@@ -1,19 +1,20 @@
 class_name RoleStats
 extends Node
 
-@export var max_hp := 3
-@export var max_mp := 3
-@export var max_experience := 100
-
+@export var max_hp: int = 3
+@export var max_mp: int = 3
+@export var max_experience: int = 3
+@export var hp_regen: int = 0
+@export var mp_regen: int = 0
 
 var grade := 1
 
-@onready var hp := max_hp:
+@onready var hp: int:
 	set(v):
 		hp = clampi(v, 0, max_hp)
-@onready var mp := max_mp:
+@onready var mp: int:
 	set(v):
 		mp = clampi(v, 0, max_mp)
-@onready var experience := max_experience:
+@onready var experience: int:
 	set(v):
 		experience = clampi(v, 0, max_experience)
